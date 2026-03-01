08f928126a4df9ab56517673daccbc3408f149f6
Pieter Cooreman <47208869+PieterCooreman@users.noreply.github.com>
2026-03-01 20:13:35 +0100

    Enhance start.bat to notify server launch
    Added server start messages and browser launch command.

--- start.bat
+++ start.bat
@@ -8,6 +8,15 @@ for /f "tokens=5" %%P in ('netstat -ano ^| findstr /i ":%PORT% " ^| findstr /i "
   taskkill /pid %%P /f >nul 2>nul
 )
 
-python ASP4/server.py 0.0.0.0 8080 www
+echo Starting ASP/PY server...
+echo Launching http://localhost:%PORT% in your default browser...
+echo.
+
+:: This command automatically opens the URL
+start "" "http://localhost:%PORT%"
+
+::set ASP_PY_LOG=1 
+::set ASP_PY_TRACE_REQUEST=1 
+python3 ASP4/server.py 0.0.0.0 8080 www	
 
 endlocal
